9018c5c70a449e389516c9a817dc4053e09e0690
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2015-09-16 14:36:03 +0200

    FC039: Node method cannot be accessed with key

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,4 @@ default['windows_packages']=[]
 default['zip_packages']=[]
 default['windows_features']={"remove"=>["MediaPlayback","WindowsMediaPlayer","MediaCenter","TabletPCOC","FaxServicesClientPackage",
         "Xps-Foundation-Xps-Viewer","Printing-XPSServices-Features"]}
-default['certificate_bundle']=["http://curl.haxx.se/ca/cacert.pem"]
-default["environment"]={}
+default.environment={}
