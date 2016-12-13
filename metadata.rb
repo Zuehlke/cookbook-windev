0cb0b12685e13234ec404207c65b8c0b282d2234
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2016-12-13 14:03:13 +0100

    bump the version to 0.6.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'var@zuehlke.com'
 license          'MIT'
 description      'Configures Windows'
 long_description 'Configures a Windows installation. Parameters include the list of Windows features to remove and data on the installer packages to be added'
-version          '0.5.1'
+version          '0.6.0'
 issues_url 'https://github.com/Zuehlke/cookbook-windev/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/Zuehlke/cookbook-windev' if respond_to?(:source_url)
 
