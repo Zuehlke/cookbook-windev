4b6153f9baa662cfa3e228d322b70950c4837d33
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2016-12-13 13:29:45 +0100

    Recipe to rerun chef after reboot Handles installations requiring multiple reboots

---
+++ recipes/auto_chef.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: windev
+# Recipe:: auto_chef
+#
+# Author:: Vassilis Rizopoulos (<var@zuehlke.com>)
+#
+# Copyright (c) 2014-2016 Zühlke, All Rights Reserved.
+
+#Create a user to restart the process after booting
+user "chef-auto-start" do
+  comment "Chef installation user"
+  password 'chef-auto-start'
+  action :create
+end
+#add the user to the administrators group
+group "Administrators" do
+  action :modify
+  members "chef-auto-start"
+  append true
+end
+#Set up autologin for chef-auto-start
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\WinLogon" do 
+  values [{:name => "AutoAdminLogin",:type => :string,:data => "1"}, 
+    {:name => "AutoAdminLogon",:type => :string,:data => "1"}, 
+    {:name => "DefaultUserName",:type => :string,:data => "chef-auto-start"},
+    {:name => "DefaultPassword",:type => :string,:data => "chef-auto-start"}
+  ]
+  action :create
+end
+
+#Register the chef command to run on startup
+cmd=ENV["CHEF_SCRIPT"]
+raise "CHEF_SCRIPT not defined, cannot register Chef for startup" unless cmd
+cmd_params=ENV["CHEF_CONFIG"]
+cmd_params||=node.fetch("chef",{})["config"]
+raise "No chef configuration attribute defined. Add node[\"chef\"][\"config\"] to your configuration or define CHEF_CONFIG" unless cmd_params
+
+windows_auto_run 'Chef Run' do
+  program cmd
+  args    "#{cmd_params}"
+  action  :create
+end
---
+++ recipes/remove_auto_chef.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: windev
+# Recipe:: auto_chef
+#
+# Author:: Vassilis Rizopoulos (<var@zuehlke.com>)
+#
+# Copyright (c) 2014-2016 Zühlke, All Rights Reserved.
+
+#Create a user to restart the process after booting
+user "chef-auto-start" do
+  comment "Chef installation user"
+  password 'chef-auto-start'
+  force true
+  action :remove
+end
+#Set up autologin for chef-auto-start
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\WinLogon" do 
+  values [{:name => "AutoAdminLogin",:type => :string,:data => "0"}, 
+    {:name => "AutoAdminLogon",:type => :string,:data => "0"}, 
+    {:name => "DefaultUserName",:type => :string,:data => ""},
+    {:name => "DefaultPassword",:type => :string,:data => ""}
+  ]
+  action :create
+end
+
+windows_auto_run 'Chef Run' do
+  program "echo"
+  args    ""
+  action  :remove
+end
