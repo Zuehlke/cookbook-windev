561378900eaf1097456a491ac6e382902dd0469a
Torben Knerr <mail@tknerr.de>
2017-08-08 13:55:27 +0200

    don't remove any windows features by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,5 @@ default['software_depot']="software"
 default['drivers']=[]
 default['installer_packages']=[]
 default['zip_packages']=[]
-default['windows_features']={"remove"=>["MediaPlayback","WindowsMediaPlayer","MediaCenter","TabletPCOC","FaxServicesClientPackage",
-        "Xps-Foundation-Xps-Viewer","Printing-XPSServices-Features"]}
+default['windows_features']={"remove"=>[]}
 default.environment={}
