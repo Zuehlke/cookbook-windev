01e0027fa2bec8bd079b8af2dc06e94c33f24a8b
Torben Knerr <mail@tknerr.de>
2017-08-08 13:52:22 +0200

    rename "windows_packages" to "installer_packages" in default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['software_depot']="software"
 default['drivers']=[]
-default['windows_packages']=[]
+default['installer_packages']=[]
 default['zip_packages']=[]
 default['windows_features']={"remove"=>["MediaPlayback","WindowsMediaPlayer","MediaCenter","TabletPCOC","FaxServicesClientPackage",
         "Xps-Foundation-Xps-Viewer","Printing-XPSServices-Features"]}
